90d4d54ac9b9d10b45d92775758d2f5c61767608
cortez <jenkinsc@dewv.edu>
2018-10-15 18:39:01 +0000

    Updates via sync script

---
+++ castleBreakIn.js
@@ -0,0 +1,11 @@
+import { game, Sprite } from "../sgc/sgc.js";
+game.setBackground("grass.png");
+class Wall extends Sprite {
+    Constructor(x, y, name, image) {
+        this.x;
+        this.y;
+        this.name="name";
+        this .setImage("grass.png");
+        this.height
+    }
+}
